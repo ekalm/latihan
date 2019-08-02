872ad784ea86547c669069063376c82f6c2dbe2a
ekalm <ekaluthfiana29@gmail.com>
2019-08-02 16:16:03 +0700

    tambahan

---
+++ perhitungan.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 5;
+    $b = 10;
+    $c = $a + $b;
+
+    if($c > 10) {
+        echo '<h1 style="color:red">' . $c . '</h1>';
+    }   elseif ($c < 10) {
+        echo '<h1 style ="color:green">' . $c . '</h1>';
+    } 
+    $d = 5;
+    $e = 1;
+    $f = $d + $e;
+    if($d > 10) {
+        echo '<h1 style="color:blue">' . $f . '</h1>';
+    }   elseif ($d < 10) {
+        echo '<h1 style ="color:green">' . $f . '</h1>';
+    } 
+    $g = 20;
+    $h = 15;
+    $i = $g + $h;
+    if($g > 10) {
+        echo '<h1 style="color:blue">' . $i . '</h1>';
+    }   elseif ($g < 10) {
+        echo '<h1 style ="color:red">' . $i . '</h1>';
+    }
+    $j = 35;
+    $k = 20;
+    $l = $j + $k;
+    if($j > 15) {
+        echo '<h1 style="color:red">' . $l . '</h1>';
+    }   elseif ($j < 15) {
+        echo '<h1 style ="color:koral">' . $l . '</h1>';
+    }
+    $m = 45;
+    $n = 30;
+    $o = $m + $o;
+    if($m > 20) {
+        echo '<h1 style="color:jingga">' . $o . '</h1>';
+    }   elseif ($m < 20) {
+        echo '<h1 style ="color:red">' . $o . '</h1>';
+    }
+    $p = 55;
+    $q = 50;
+    $r = $p + $q;
+    if($p > 40) {
+        echo '<h1 style="color:cyan">' . $r . '</h1>';
+    }   elseif ($p < 35) {
+        echo '<h1 style ="color:red">' . $r . '</h1>';
+    }
+    $s = 40;
+    $t = 30;
+    $u = $s + $t;
+    if($s > 20) {
+        echo '<h1 style="color:slateblue">' . $u . '</h1>';
+    }   elseif ($s < 20) {
+        echo '<h1 style ="color:red">' . $u . '</h1>';
+    }
+    $v = 60;
+    $w = 50;
+    $x = $v + $w;
+    if($v > 25) {
+        echo '<h1 style="color:lime">' . $x . '</h1>';
+    }   elseif ($v < 25) {
+        echo '<h1 style ="color:red">' . $x . '</h1>';
+    }
+    $y = 70;
+    $z = 50;
+    $ab = $s + $t;
+    if($y > 40) {
+        echo '<h1 style="color:indigo">' . $ab . '</h1>';
+    }   elseif ($y < 40) {
+        echo '<h1 style ="color:red">' . $ab . '</h1>';
+    }
+    $ac = 100;
+    $ad = 80;
+    $ae = $s + $t;
+    if($ac > 50) {
+        echo '<h1 style="color:violet">' . $ae . '</h1>';
+    }   elseif ($ac < 50) {
+        echo '<h1 style ="color:red">' . $ae . '</h1>';
+    }
+
+
+
+    
+
+    
+    ?>
+</body>
+</html>
---
+++ umur.php
@@ -0,0 +1,9 @@
+<?php
+$tama = 1;
+$toni = 0.5;
+$tinitama = 14;
+$tinitoni = 34;
+
+echo "umur Tama = $tamaX<br>";
+echo "umur Toni = $toniX<br>";
+echo "umur Toni + umur Tini =$tinitoni<br>";
